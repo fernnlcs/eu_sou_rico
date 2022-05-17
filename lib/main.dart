f79c52ec43b1dca33d1a08459fe59f28ee81cbe8
Fernando Silva <fernnlcs@gmail.com>
2022-05-17 17:55:17 -0300

    Feat: Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,19 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Olá mundo!'),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
+          title: const Text('Eu sou rico'),
+        ),
+        body: const Center(
+          child: Image(
+            image: NetworkImage(
+              'https://www.hardware.com.br/static/wp/2021/03/24/descobrir-musica.jpg',
+            ),
+          ),
+        ),
       ),
     ),
   );
